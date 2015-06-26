f80cfddff66739008802d4d6572bec006072dc32
hitoy <vip@hitoy.org>
2015-06-26 13:55:45 +0800

    sitemap update

--- auto website/templates/sitemap.php
+++ auto website/templates/sitemap.php
@@ -3,9 +3,9 @@ ob_start();
 header("Content-Type:text/xml;Charset=utf-8");
 $sitemap = $list->showall();
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 foreach($sitemap as $url){
 echo "<url><loc>".host.$url.".html</loc></url>";
 }
