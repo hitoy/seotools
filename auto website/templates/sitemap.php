b08dffe870c11c3bba63304b3ac4e310919775cf
hitoy <vip@hitoy.org>
2015-06-26 13:31:24 +0800

    sitemap upate

--- auto website/templates/sitemap.php
+++ auto website/templates/sitemap.php
@@ -7,7 +7,7 @@ $sitemap = $list->showall();
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php
 foreach($sitemap as $url){
-echo "<url><loc>".host.$url."</loc></url>";
+echo "<url><loc>".host.$url.".html</loc></url>";
 }
 ?>
 </urlset>
