f985e0f2dd149d8c28caf27eaa94a2b78612ad07
hitoy <vip@hitoy.org>
2016-01-19 17:42:47 +0800

    upnew

--- auto website/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("webname","Industrial Boiler Expert");
-define("lang","en-US");
-define("charset","utf-8");
-define("host",$_SERVER["HTTP_HOST"]);
-define("keyfile",ABPATH."/data/key.txt");
--- auto website/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("./init.php");
-/* $router 路由类
- * $list   列表类
- *
- */
-$router->route();
